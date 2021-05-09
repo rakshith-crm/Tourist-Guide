2464b916ed19721bb09b6433766c790bfc45607a
Rakshith C.R.M <rakshithcrm@gmail.com>
2021-05-09 10:47:10 +0530

    screenshots and new location

--- server/schema.sql
+++ server/schema.sql
@@ -41,7 +41,8 @@ insert into ratings values(1, '6a93b3a4-72c8-4953-92e9-4e923ff38590', 'true');
 --used selects
 select locations.location_id, location_name, latitude, longitude, description, city, photo, visit_during, username, liked from locations left join users on users.user_id=locations.added_by left outer join ratings on ratings.location_id = locations.location_id and ratings.user_id=cast('fd1a6c3b-32c6-4f4b-940d-9135be254967' as uuid);
 
-select count(*) from ratings group by location_id;
+
+select location_id,count(*) from ratings group by location_id;
 
 select table1.*,table2.count from (select locations.location_id, location_name, latitude, longitude, description, city, photo, visit_during, username, liked from locations left join users on users.user_id=locations.added_by left outer join ratings on ratings.location_id = locations.location_id and ratings.user_id=cast('6a93b3a4-72c8-4953-92e9-4e923ff38590' as uuid)) as table1 inner join 
 (select location_id,count(*) from ratings group by location_id) as table2 on table1.location_id = table2.location_id;
